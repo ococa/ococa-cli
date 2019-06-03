afe66c9bf6c484b47ae3df95bd26789379f84e80
wangchao <wangchao.personal@gmail.com>
2019-06-03 17:27:32 +0800

    bug fix

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const ora = require('ora');
 const chalk = require('chalk');
 const symbols = require('log-symbols');
 
-program.version('1.0.0', '-v, --version')
+program.version('1.0.5', '-v, --version')
 	.command('init <name>')
     .action((name) => {
         if(!fs.existsSync(name)){
@@ -24,7 +24,7 @@ program.version('1.0.0', '-v, --version')
             ]).then((answers) => {
                 const spinner = ora('正在下载模板...');
                 spinner.start();
-				download('https://github.com/ococa/scaffolds.git', name, {clone: true}, (err) => {
+				download('ococa/scaffolds', name, {clone: true}, (err) => {
                     if(err){
                         spinner.fail();
                         console.log(symbols.error, chalk.red(err));
